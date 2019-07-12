8b231b009cafe88423b20957c318ffb21260859e
Phil.Chang <wen-chuan.chang@senao.com>
2019-07-12 15:20:29 +0800

    First commit.

---
+++ IRTabbedPageViewController/IRTabbedPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  IRTabbedPageViewController.h
+//  IRTabbedPageViewController
+//
+//  Created by Phil on 2019/7/12.
+//  Copyright © 2019 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRTabbedPageViewController.
+FOUNDATION_EXPORT double IRTabbedPageViewControllerVersionNumber;
+
+//! Project version string for IRTabbedPageViewController.
+FOUNDATION_EXPORT const unsigned char IRTabbedPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRTabbedPageViewController/PublicHeader.h>
+
+
